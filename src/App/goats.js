1f7e25f0dea487b60cbc4a70a8479af8e5896ed8
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-06-17 19:22:35 -0500

    goat array

---
+++ src/App/goats.js
@@ -0,0 +1,56 @@
+
+const goats = [
+  {
+    id: 'goat1',
+    name: 'billy',
+    disposition: 'rude',
+    imageUrl: 'https://www.telegraph.co.uk/content/dam/pets/2016/08/11/103675361_jack-the-goat-PETS_trans_NvBQzQNjv4BqZgEkZX3M936N5BQK4Va8RWtT0gK_6EfZT336f62EI5U.jpg?imwidth=450',
+    favPose: 'downward facing dog',
+    isFainting: true,
+    fullness: 67,
+    color: 'brown',
+  },
+  {
+    id: 'goat2',
+    name: 'lil billy',
+    disposition: 'chill',
+    imageUrl: 'https://images2.minutemediacdn.com/image/upload/c_crop,h_843,w_1500,x_0,y_0/f_auto,q_auto,w_1100/v1555172619/shape/mentalfloss/iStock-177369626_0.jpg',
+    favPose: 'cobra',
+    isFainting: true,
+    fullness: 23,
+    color: 'white',
+  },
+  {
+    id: 'goat3',
+    name: 'big billy',
+    disposition: 'sunny',
+    imageUrl: 'https://i.ytimg.com/vi/O3NQzrkAhD4/maxresdefault.jpg',
+    favPose: 'headstand',
+    isFainting: true,
+    fullness: 6,
+    color: 'mixed',
+  },
+  {
+    id: 'goat4',
+    name: 'medium billy',
+    disposition: 'blah',
+    imageUrl: 'https://image.shutterstock.com/image-photo/grey-horned-goat-grazing-on-260nw-379612288.jpg',
+    favPose: 'upward facing dog',
+    isFainting: true,
+    fullness: 67,
+    color: 'grey',
+  },
+  {
+    id: 'goat5',
+    name: 'silly',
+    disposition: 'goofy',
+    imageUrl: 'https://i.pinimg.com/originals/d2/db/24/d2db24911e52165293150439f03a4e11.jpg',
+    favPose: 'childs pose',
+    isFainting: true,
+    fullness: 34,
+    color: 'black',
+  },
+];
+
+
+export default goats;
